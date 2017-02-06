141e26f61bd813096b53b05d978d09eb2e2345ba
Jordanbana <jordanbana@gmail.com>
2017-02-06 00:45:43 -0800

    added bootstrap

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <html>
   <head>
     <meta charset="utf-8">
@@ -8,8 +13,9 @@
 
     <br>
 
+    <div class="container">
 
-      Movies for Search '{{$search}}':
+      <h2>Movies for Search '{{$search}}':</h2>
       @foreach($dvds as $dvd)
         <ul>
             <li><b>{{ $dvd->title }}</b></li>
@@ -17,6 +23,8 @@
             <li>{{ $dvd->genre_name}}</li>
         </ul>
       @endforeach
+
+    </div>
 
   </body>
 </html>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <html>
   <head>
     <meta charset="utf-8">
     <title>DVD Search</title>
   </head>
   <body>
-    <h1> DVD Search</h1>
-    <form action="/dvds" method="get">
-      DVD: <input type="text" name="dvd"?
-      <br>
-      <button type="submit">Search</button>
-    </form>
+    <div class="container">
+      <h1> DVD Search</h1>
+
+      <form action="/dvds" method="get">
+        DVD: <input type="text" name="dvd"?
+        <br>
+        <button type="submit">Search</button>
+      </form>
+      
+      </div>
   </body>
 </html>
